4983c1ecee492dc10b714303f3f1e9f8e0cdbfdd
lavanganji <lavanganji@gmail.com>
2025-08-05 19:17:21 -0500

    Problem1

--- Problem1.java
+++ Problem1.java
@@ -21,27 +21,111 @@ In the online assesment this problem was sort of tagged as approximate solution,
 Awaiting for the result!
  */
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem1 {
 
-    int arr1[][] = { {1, 2000}, {2, 3000}, {3, 4000}, {4, 2000} };
-    int arr2[][] = { { 1, 5000 }, {2, 3000} };
-
-     List<int[]> re = getClosestPairs(arr1, arr2, 5000);
+   
 
     public static void main(String[] args) {
-        
+        int arr1[][] = { {1, 2000}, {2, 3000}, {3, 4000}, {4, 2000} };
+        int arr2[][] = { { 1, 5000 }, {2, 3000} };
+
+        List<int[]> re = getClosestPairs(arr1, arr2, 5000);
+
+        for(int[] pair : re) {
+            System.out.println("{" + pair[0] + ", " + pair[1] + "}");
+        }
+        System.out.println("Time Comlexity for getClosestPairs() solution is O(n^2) where n is the number of elements in the array ");
+            
+
+        List<int[]> reOptimized = getClosestPairsOptimized(arr1, arr2, 5000);
+        for(int[] pair : reOptimized) {
+            System.out.println("{" + pair[0] + ", " + pair[1] + "}");
+        }
+        System.out.println("Time Comlexity for getClosestPairsOptimized() solution is O(n log n) where n is the number of elements in the array ");
     }
-    public List<int[]> getClosestPairs(int[][] arr1, int[][] arr2, int target) {
+    public static List<int[]> getClosestPairs(int[][] arr1, int[][] arr2, int target) {
         // Your implementation here
         // Find the pairs from both the arrays whose vaue add upto a sum which 
         // is less than given target and should be closest to the target.
-
         
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int[] p : arr1){
+            int id = p[0];
+            int value = p[1];
+            map.put(id, value);
+        }
+      
 
 
 
-        return null; // Placeholder return statement
-    } 
+        int closestSum = Integer.MIN_VALUE;
+        List<int[]> result = new ArrayList<>();
+
+        for(int[] p : arr2){
+            int id = p[0];
+            int value = p[1];
+
+            for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+                int sum = entry.getValue() + value;
+              // System.out.println("Sum: " + sum + " for pair: " + entry.getKey() + ", " + id);
+                if(sum <= target && sum >= closestSum && closestSum<= target){
+                    closestSum = sum;
+                    result.add(new int[]{entry.getKey(), id});
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static List<int[]> getClosestPairsOptimized(int[][] arr1, int[][] arr2, int target) {
+        // Your implementation here
+        // Find the pairs from both the arrays whose vaue add upto a sum which 
+        // is less than given target and should be closest to the target.
+        
+        List<int[]> result = new ArrayList<>();
+        int closestSum = Integer.MIN_VALUE;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int[] p : arr1){
+            int id = p[0];
+            int value = p[1];
+            map.put(value, id);
+        }
+
+        //finding the closest search for the second array
+        for(int[] p : arr2){
+            int id = p[0];
+            int value = p[1];
+
+            int findElement = target - value;
+            if( map.containsKey(findElement)){
+                int[] pair = {map.get(findElement), id};
+                result.add(pair);
+            }
+            else {
+                int closestValue = Integer.MAX_VALUE;
+                for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+                    int sum = entry.getKey() + value;
+                    if(sum <= target && sum <= closestSum && closestSum<= target){
+                        closestSum = sum;
+                        closestValue = entry.getKey();
+                    }
+                }
+                if(closestValue != Integer.MAX_VALUE) {
+                    result.add(new int[]{map.get(closestValue), id});
+                }
+            }
+            
+        }
+
+        return result;
+    }
 }
